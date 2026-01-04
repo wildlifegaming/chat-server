3d418cceb4b0ec7100823bb17cf8cde32547634e
wildlifegaming <nico.bayerl11@gmail.com>
2026-01-04 16:06:53 +0100

    Update chat_server.py (06)

--- chat_server.py
+++ chat_server.py
@@ -1440,6 +1440,7 @@ def snake_score():
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port, debug=False)
+    socketio.run(app, host="0.0.0.0", port=port, debug=False, allow_unsafe_werkzeug=True)
+
 
 
